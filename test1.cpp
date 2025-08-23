edf12855f724d3eade349439467e82a35e92b28c
hillomar <tamimomar86@gmail.com>
2025-08-23 22:06:43 +0600

    sdnigbjivbdfjigbubu

--- test1.cpp
+++ test1.cpp
@@ -1,34 +1,6 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int x;
-    cin>>x;
-    int a = 1;
-    int count = 0;
-    while(1){
-        while(1){
-        if(a<x){
-            a*=2;
-        }
-        else if(a==x){
-            a==0;
-            break;
-        }
-        else{
-            a/=2;
-            break;
-        }
-    }
-    if(a!=0){
-        
-        x -= a;
-        a==1;
-    }
-    else{
-        count+=1;
-        goto here;
-    }
-    }
-    here:
-    cout<<count<<'\n';
+
+int main() {
+    
 }
