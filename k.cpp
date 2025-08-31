280558c07dbd320c1051796bc121ad11c730182c
hillomar <tamimomar86@gmail.com>
2025-08-31 22:15:02 +0600

    new

--- k.cpp
+++ k.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
 using namespace std;
-
-int odd_divisor(int n){
-    int divisor = 0;
-    for(int i = 3;i<=n;i+=2){
-        if(n%i==0){
-            divisor = i;
+int check(int array[],int size,int number){
+    int d = 0;
+    for(int i=0;i<size;i++){
+        if(array[i]==number){
+            d+=1;
             break;
         }
     }
-    return divisor;
+    return d;
 }
-
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int d = 0;
-        while(1){
-            int div = odd_divisor(n);
-            if(div%2!=0&&n%div==0){
-                n/=div;
-                d+=1;
-            }
-            else if(n>1){
-                n-=1;
-                d+=1;
-            }
-            else{
-                break;
-            }
-        }
-        if(d%2!=0){
-            cout<<"Ashishgup"<<'\n';
-        }
-        else{
-            cout<<"FastestFinger"<<'\n';
+    int n;
+    cin>>n;
+    int array[n];
+    int result = 0;
+    for(int i = 0;i<n;i++){
+        cin>>array[i];
+        int dool = check(array,i,array[i]);
+        if(dool==0){
+            result+=1;
         }
     }
+    cout<<result<<'\n';
 }
