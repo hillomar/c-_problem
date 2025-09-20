dfe05b8c5399a0e298151fffaeab811973634eb6
hillomar <tamimomar86@gmail.com>
2025-09-21 01:51:14 +0600

    new

--- yo.cpp
+++ yo.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int array[n*2];
-    for(int i = 0;i<n*2;i++){
-        cin>>array[i];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        string s;
+        cin >> s;
+        int n = s.size();
+
+        int cnt0 = 0, cnt1 = 0;
+        for (char c : s) {
+            if (c == '0') cnt0++;
+            else cnt1++;
+        }
+
+        int used = 0;
+        for (int i = 0; i < n; i++) {
+            int want = (s[i] == '0' ? 1 : 0); // need the opposite
+            if (want == 0 && cnt0 > 0) {
+                cnt0--;
+                used++;
+            } else if (want == 1 && cnt1 > 0) {
+                cnt1--;
+                used++;
+            } else {
+                break; // can't continue
+            }
+        }
+
+        cout << (n - used) << "\n";
     }
-    int count = 0;
-    for(int j = 0;j<n*2;j+=2){
-        
-    }
+
+    return 0;
 }
