d9bddddf5779f86212268ea0f914ae7a5814caf6
Mohammad Abdulla Hill Omar <ineedtochangefr@gmail.com>
2025-10-08 07:06:01 +0600

    Meal er hishab final yo.cpp

--- yo.cpp
+++ yo.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void calculation(float meal,float joma,float &pabe,float &pabo,float khori,int &pacce,int &dicce,float &total_khoroch){
-    float total = (meal*83.6)+250+khori;
+    float total = (meal*83.2)+250+khori;
     total_khoroch+=total;
     if(total>=joma){
         pabo+=total-joma;
@@ -15,7 +15,7 @@ void calculation(float meal,float joma,float &pabe,float &pabo,float khori,int &
 }
 
 int main() {
-    float bordar = 40;
+    float bordar = 42;
     float pabe = 0;
     float pabo = 0;
     float khori = 220;
@@ -25,19 +25,40 @@ int main() {
     int total_joma = 0;
     float total_khoroch = 0;
     float total_meal = 0;
+    int serial = 1;
     while(bordar--){
         float meal,joma;
         cin>>meal>>joma;
         total_meal+=meal;
         total_joma+=joma;
-        if(meal<11){
-            khori = 120;
-            com_dicce+=1;
+        if(meal<11&&meal>6){
+            khori = 110;
+        }
+        else if(meal<6&&meal>3){
+            khori = 70;
+        }
+        else if(meal<=3){
+            khori = 30;
+        }
+        else if(meal==15){
+            khori = 180;
+        }
+        else if(meal<15&&meal>11){
+            khori = 170;
+        }
+        else if(meal>15&&meal<18){
+            khori = 200;
         }
         else{
             khori = 220;
         }
+        int minus = joma - ((meal*83.2)+250+khori);
+        int plus = ((meal*83.2)+250+khori) - joma;
         calculation(meal,joma,pabe,pabo,khori,pacce,dicce,total_khoroch);
+        cout<<"No: "<<serial<<"\nkhoroch: "<<int((meal*83.2)+250+khori);
+        (((meal*83.2)+250+khori))<joma ? cout<<" Pabe: "<<minus : cout<<" Pabo: "<<plus;
+        cout<<'\n'<<'\n';
+        serial+=1;
     }
     cout<<"Total meal: "<<total_meal<<'\n';
     cout<<"Total joma: "<<total_joma<<'\n';
