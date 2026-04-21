0089fa67a74294d77bef29035eec18a9fdeafcd7
hillomar <tamimomar86@gmail.com>
2026-04-21 12:52:00 +0600

    new

---
+++ class_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+    string name;
+    public:
+    Person(string a){
+        name = a;
+    }
+    Person(Person &c){
+        name = c.name;
+    }
+    void nameset(string a){
+        name = a;
+    }
+    void display(int g){
+        cout<<"Person "<<g<<": "<<name<<'\n';
+    }
+};
+
+int main(){
+    Person P1 = {"AAA"};
+    Person P2 = P1;
+    P1.display(1);
+    P2.display(2);
+    P2.nameset("BBB");
+    cout<<"After Modifying Person 2:\n";
+    P1.display(1);
+    P2.display(2);
+
+
+}
